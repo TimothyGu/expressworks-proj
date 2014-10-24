edca7d5ad8980722c4bd9d08d6fe924dc762267d
Timothy Gu <timothygu99@gmail.com>
2014-10-23 20:08:55 -0400

    Add static
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

---
+++ static.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+/*
+ * Solution to "Static" exercise
+ * Copyright (c) 2014, Tiancheng "Timothy" Gu
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * This is even simpler than the one we did before, because there isn't any
+ * routing to worry about.
+ */
+
+var express = require('express')                                              
+var app = express()
+
+app.use(express.static(process.argv[3]  // If a file is supplied through CLI,
+                                        // use that, or else ...
+    || path.join(__dirname, 'public'))) // Use the one in 'public' directory
+
+// Port is specified by `process.argv[2]`
+app.listen(Number(process.argv[2]))
